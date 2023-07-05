517afa808f31fb50723d3f19b957f86a4f12a205
avanish-srivastava <avanish.srivastava@nirvanasolutions.com>
2023-07-05 14:06:53 +0530

    Added advance openfin popup

---
+++ src/pages/PopupWindow.tsx
@@ -0,0 +1,11 @@
+const PopupWindow = () => {
+  return (
+    <div className="heading">
+      <h1>Sample Popup</h1>
+      <div>
+        <p>This is a same popup</p>
+      </div>
+    </div>
+  );
+};
+export default PopupWindow;
