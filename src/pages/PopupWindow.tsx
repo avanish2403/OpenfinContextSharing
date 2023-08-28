72cb23acbe06c21ddc74b6e7157d024fbb080a49
avanish-srivastava <avanish.srivastava@nirvanasolutions.com>
2023-08-28 16:40:02 +0530

    Added popup

--- src/pages/PopupWindow.tsx
+++ src/pages/PopupWindow.tsx
@@ -1,4 +1,9 @@
 const PopupWindow = () => {
+  const finWindow = fin.Window.getCurrentSync();
+  finWindow.addListener('close-requested', function (event) {
+    // alert('close requested');
+    finWindow.close(true);
+  });
   return (
     <div className="heading">
       <h1>Sample Popup</h1>
